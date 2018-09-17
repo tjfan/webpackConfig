12ef4a44e36333fb4c0e2b369d40d5b1400ada30
methods <async_wait@163.com>
2018-09-17 16:46:09 +0800

    Update webpack.dev.conf.js

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,24 +2,46 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.conf');
+const baseWebpackConfig = require('./webpack.base.conf');  // 引入基础配置
+const config = require('./config');
+
 
 module.exports = merge(baseWebpackConfig, {
-  // mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'development',
+  devtool: config.dev.devtool,  // 开启sourceMap,生产环境下不使用
   devServer: {
-    contentBase: path.resolve(__dirname, '../dist'),
-    port: 8080,
-    host: 'localhost',
-    overlay: true,
-    compress: true, // 可选，压缩
-    open: true,
-    hot: true,
-    inline: true,
-    progress: true
+    contentBase: './src/',
+    open: false,
+    host: config.dev.host,
+    port: config.dev.port,
+    useLocalIp: false,  // 允许浏览器使用本地ip打开
+    hot: true,  // 配合webpack.NamedModulesPlugin、webpack.HotModuleReplacementPlugin完成
+    compress: true,  // 一切服务都启用gzip压缩
+    quiet: false, // 启动quiet后，除了初始启动信息以外的任何内容都不会被打印到node控制台
+    overlay: true,  // 在浏览器中显示编译错误
+    inline: true, // 内敛模式 实时重载的脚本插入到你的（bundle）中，并且构建消息将出现在浏览器控制台
+    clientLogLevel: 'warning', // 内联模式  那些消息将出现在浏览器控制台
+
+    historyApiFallback: {  // 当使用HTML5 History Api 时，任意的404响应是否被替代为index.html
+      rewrites: [
+        {
+          from: /\.*/,
+          to: path.posix.join(config.dev.assetsPublicPath, 'html/index.html')
+        }
+      ]
+    },
+    publicPath: config.dev.assetsPublicPath,
+    proxy: config.dev.proxyTable
   },
   plugins: [ 
     new webpack.HotModuleReplacementPlugin(), // 启动热更新
-    new webpack.NamedChunksPlugin()
+    // new webpack.NamedChunksPlugin(),
+    new webpack.NameModulesPlugin(),  // 开启HMR时显示模块的相对路径，建议用于开发环境
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"'
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin()  // 在编译出现错误的时候，使用NoEmitOnErrorsPlugin来跳出输出阶段，这样可以确保输出资源不会包含错误
   ]
 })
