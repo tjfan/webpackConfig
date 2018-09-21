812990e7584889aa6faa3ba31b4ea2698ea5eac2
methods <async_wait@163.com>
2018-09-21 14:58:58 +0800

    Update webpack.prod.conf.js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,17 +1,29 @@
 'use strict'
 const path = require('path');
-const merge = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.conf');
+const baseWebpackConfig = require('./webpack.base.conf'); // 基础配置
+const config = require('./config'); // 配置
 const webpack = require('webpack');
-const env = require('../config/prod.js');
+const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UgligyJSPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // 拷贝文件
 const HappyPack = require('happypack');
 
+process.env.NODE_ENV = 'production'
 
 const webpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
   devtool: 'source-map',
+  output : {
+    publicPath: config.build.assetsPublicPath,
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].[chunkhash:8].js',
+    chunkFilename: 'js/[id].[chunkhash:7].js'
+  },
+  module: {
+    rules: []
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
@@ -20,51 +32,45 @@ const webpackConfig = merge(baseWebpackConfig, {
     new CleanWebpackPlugin(['dist'], {
       root: path.resolve(__dirname, '../')
     }),
-    new HtmlWebpackPlugin({
-      template: './src/html/index.html',
-      hash: true,
-      minify: {
-        removeAttributeQuotes: true  // 去掉引号，减小文件大小
-      }
-    }),
     // 提取css文件
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css'
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[id].[contenthash:8].css'
     }),
     // 开启多个进程
-    new HappyPack({
-      id: 'babel',
-      threads: 4,
-      loaders: ['loader-babel']
-    }),
+    // new HappyPack({
+    //   id: 'babel',
+    //   threads: 4,
+    //   loaders: ['loader-babel']
+    // }),
     // 作用域提升
     new webpack.optimize.ModuleConcatenationPlugin(),
-    new webpack.DllPlugin({
-      path: path.resolve(__dirname, './dist/dll', 'manifest.json'),
-    })
+    // new webpack.DllPlugin({
+    //   path: path.resolve(__dirname, './dist/dll', 'manifest.json'),
+    // })
   ],
   // 提取公共代码
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks: 'all',
-          name: 'vendors',
-        },
-        commons: {
-          chunks: "async",
-          name: "common-async",
-          minSize: 0,
-          minChunks: 2
-        }
-      }
-    }
-  },
-  runtimeChunk: {
-    name: "manifest"
-  }
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all',
+  //     cacheGroups: {
+  //       vendors: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         chunks: 'all',
+  //         name: 'vendors',
+  //       },
+  //       commons: {
+  //         chunks: "async",
+  //         name: "common-async",
+  //         minSize: 0,
+  //         minChunks: 2
+  //       }
+  //     }
+  //   }
+  // },
+  // runtimeChunk: {
+  //   name: "manifest"
+  // }
 })
 
 module.exports = webpackConfig;
